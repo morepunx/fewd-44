e1c45c93af467f364059a181cafdb9b8849809a1
morepunx <killian.escobedo@gmail.com>
2016-05-10 18:38:49 -0700

    Lotsa work since last thursday’s class

---
+++ travel-news/js/travel-news.js
@@ -0,0 +1,8 @@
+var child = document.createElement("li");
+
+var parent = document.querySelector(".sponsored-articles");
+
+parent.appendChild(child);
+
+child.textContent = "Trump wins 37 states! New USA PRESIDENT!"
+"Trump wins 37 states! New USA PRESIDENT!";
