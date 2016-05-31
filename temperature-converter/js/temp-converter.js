ce6ccb4f09fcab39b5bdab9e768b41cc3a9a5d27
morepunx <killian.escobedo@gmail.com>
2016-05-30 22:49:29 -0700

    updates to temp converter

--- temperature-converter/js/temp-converter.js
+++ temperature-converter/js/temp-converter.js
@@ -6,16 +6,17 @@ var farenheitValue = document.querySelector(".farenheit-value");
 var convertFarenheit = document.querySelector(".convert-farenheit");
 var celsiusValue = document.querySelector(".celsius-value");
 var convertCelsius = document.querySelector(".convert-celsius");
+var roundValues = document.querySelector(".round-values");
+var isChecked = roundValues.checked;
 
 
 //Events 
 convertFarenheit.addEventListener("click", farenheitToCelsius);
 convertCelsius.addEventListener("click", celsiusToFarenheit);
 
-
 //Event handeler 
 function farenheitToCelsius() {
-	console.log("running farenheitToCelsius")
+	console.log("running farenheitToCelsius");
 	
 	//get farenheit value and store it 
 	var x = parseInt(farenheitValue.value);
@@ -43,3 +44,24 @@ console.log("Running Celsius to farenheit function");
  	//update the page 
  	farenheitValue.value = cz;
 }
+
+
+function roundAllNumbers() {
+	{
+		console.log("checked?");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
