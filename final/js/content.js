6845e8d53b1fec8e9579183663f5c399f1040844
morepunx <killian.escobedo@gmail.com>
2016-07-05 21:26:03 -0700

    lotsa stuff

--- final/js/content.js
+++ final/js/content.js
@@ -3,10 +3,10 @@ var cigarettes = {
 	"description": "Small white cylinders that you light on fire and suck chemical filled smoke from",
 	"image_url": "url(img/cigarettes.jpg)",
 	"more_info_url": "https://en.wikipedia.org/wiki/Cigarette",
-	pros: [],
-	cons: [],
+	pros: ["pro item1"],
+	cons: ["con item1"],
 	friend_status: false,
-	foe_status: false
+	foe_status: true
 }
 
 var sun = {
@@ -14,8 +14,8 @@ var sun = {
 	"description": "The star that mocks us from 90 million miles away",
 	"image_url": "url(img/sun.jpg)",
 	"more_info_url": "https://en.wikipedia.org/wiki/Sun",
-	pros: [],
-	cons: [],
+	pros: ["pro item1"],
+	cons: ["con-item1"],
 	friend_status: false,
 	foe_status: false
 }
@@ -26,12 +26,35 @@ var cookies = {
 	"image_url": "url(img/cookies.jpg)",
 	"more_info_url": "https://en.wikipedia.org/wiki/Cookie",
 	pros: ["taste sweet","Chocolate"],
-	cons: ["diabetes"],
+	cons: ["diabetes","make you fat"],
+	friend_status: true,
+	foe_status: false
+}
+
+
+var swimmingPool = {
+	"name": "swimming pools",
+	"description": "holes in ground containing atmosphere juice",
+	"image_url": "url(img/pool.jpg)",
+	"more_info_url": "https://en.wikipedia.org/wiki/Swimming_pool",
+	pros: ["Sweet relief on a hot day","wet"],
+	cons: ["sunburns","drowning"],
+	friend_status: false,
+	foe_status: false
+}
+
+var babies = {
+	"name": "babies",
+	"description": "Tiny humans",
+	"image_url": "url(img/babies.jpg)",
+	"more_info_url": "https://en.wikipedia.org/wiki/Infant",
+	pros: ["?????"],
+	cons: ["crying","poop"],
 	friend_status: false,
 	foe_status: false
 }
 
 
 var json = {
-	"thing": [cigarettes,sun,cookies]
+	"thing": [cigarettes,sun,cookies,swimmingPool,babies]
 };
--- final/js/friend-or-foe.js
+++ final/js/friend-or-foe.js
@@ -11,48 +11,220 @@ var proList = document.querySelector(".pro-list");
 var conForm = document.querySelector(".con-form");
 var conInput = document.querySelector(".con-input");
 var conList = document.querySelector(".con-list");
+var friendButton = document.querySelector(".friend-button");
+var foeButton = document.querySelector(".foe-button");
+var pedestalList = document.querySelector(".pedestal-list");
+var hatePitList = document.querySelector(".hate-pit-list");
+var moreInfo = document.querySelector(".more-info");
+var differentThing = document.querySelector(".different-thing");
+var makeOwn = document.querySelector(".make-own");
+var modal = document.getElementById('myModal');
+var closeIt = document.getElementsByClassName("close")[0];
+var thingToLoad;
 
-//decide what thing to load
-var randomThing = json.thing[Math.floor(Math.random() * json.thing.length)];
+
+
+//create glory/hatepit array 
+var gloryPedestal = [];
+var hatePit = [];
+
+
 
 //event handlers 
 proForm.addEventListener("submit",submitProForm);
 conForm.addEventListener("submit",submitConForm);
-addEventListener("load",loadThing(randomThing));
+friendButton.addEventListener("click",sendToGlory);
+foeButton.addEventListener("click",sendToHatePit);
+makeOwn.addEventListener("click",openModal);
+closeIt.addEventListener("click",closeModal);
 
+
+
+window.addEventListener("load", pullFromGloryPedestal);
+window.addEventListener("load", pullFromHatePit);
+window.addEventListener("load",loadThing);
+
+
+
+//load glory pedestal from local storage 
+function pullFromGloryPedestal(){
+	if (localStorage.getItem("gloryPedestal") == null) {
+		localStorage.setItem("gloryPedestal", JSON.stringify(gloryPedestal));
+		console.log("setting glory Pedestal");
+	}
+	gloryPedestal = JSON.parse(localStorage.getItem('gloryPedestal'));
+	gloryPedestal.forEach(loadGloryPedestal);
+}
+
+function pullFromHatePit(){
+
+	if (localStorage.getItem("hatePit") == null) {
+		localStorage.setItem("hatePit", JSON.stringify(hatePit));
+		console.log("setting hate pit");
+	}  
+
+	hatePit = JSON.parse(localStorage.getItem('hatePit'));
+	hatePit.forEach(loadHatePit);
+}
+
+//opens modal for adding own thing 
+function openModal(){
+	modal.style.display = "block";
+}
+
+function closeModal(){
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+//submits pro list items 
 function submitProForm(e) {
 	console.log("fn submitProForm");
 	e.preventDefault();
 
-	json.thing[2].pros.push(proInput.value);
+	thingToLoad.pros.push(proInput.value);
 
 	buildProList();
 	proInput.value = "";
 }
 
-
+//submits con list items
 function submitConForm(e) {
 	console.log("fn submitConForm");
 	e.preventDefault();
 
-	//sun.cons.push(conInput.value);
+	thingToLoad.cons.push(conInput.value);
 
 	buildConList();
 	conInput.value = "";
 }
 
-//update functions
+//Load a new thing
 
-function loadThing(thing) {
+
+
+function loadThing() {
+
+	//decide what thing to load
+	thingToLoad = json.thing[Math.floor(Math.random() * json.thing.length)];
+	var thing = thingToLoad;
+
 	thingName.textContent= thing.name;
 	thingDescription.textContent= thing.description;
 	body.style.backgroundImage = thing.image_url;
+	moreInfo.setAttribute("href", thing.more_info_url);
 
-	thing.pros.forEach(buildProList);
-	thing.cons.forEach(buildConList);
+	thing.pros.forEach(loadPros);
+	thing.cons.forEach(loadCons);
+	
 }
 
 
+//Loads any pros stored in the array
+function loadPros(proItem) {
+	li = document.createElement("li");
+	li.textContent = proItem;
+	proList.appendChild(li);
+}
+
+//Loads any cons stored in the array
+function loadCons(conItem){
+	li = document.createElement("li");
+	li.textContent = conItem;
+	conList.appendChild(li);
+}
+
+function loadGloryPedestal(thing) {
+
+	//create li
+	li = document.createElement("li");
+	li.textContent = thing.name;
+	pedestalList.appendChild(li);
+
+	//add dropdown
+	dropdown = document.createElement("div");
+	dropdown.classList.add("dropdown");
+	li.appendChild(dropdown);
+
+
+	//add vertical ellipsis 
+	vEllipsis = document.createElement("i"); 
+	vEllipsis.classList.add("fa");
+	vEllipsis.classList.add("fa-ellipsis-v");
+	vEllipsis.setAttribute("aria-hidden","true");
+	dropdown.appendChild(vEllipsis);
+
+	//add content menu
+	dropdownContent = document.createElement("div");
+	dropdownContent.classList.add("dropdown-content");
+	dropdown.appendChild(dropdownContent);
+
+	//add links 
+	aLinkReload = document.createElement("a");
+	aLinkReload.classList.add("reload");
+	aLinkReload.textContent = "Restart";
+	dropdownContent.appendChild(aLinkReload);
+
+
+	aLinkRemove = document.createElement("a");
+	aLinkRemove.classList.add("remove");
+	aLinkRemove.textContent = "Remove";
+	dropdownContent.appendChild(aLinkRemove);
+
+
+	aLinkReload.addEventListener("click", restartThing);
+	aLinkRemove.addEventListener("click", removeThing);
+}
+
+function loadHatePit(thing) {
+
+	//create li
+	li = document.createElement("li");
+	li.textContent = thing.name;
+	hatePitList.appendChild(li);
+
+	//add dropdown
+	dropdown = document.createElement("div");
+	dropdown.classList.add("dropdown");
+	li.appendChild(dropdown);
+
+
+	//add vertical ellipsis 
+	vEllipsis = document.createElement("i"); 
+	vEllipsis.classList.add("fa");
+	vEllipsis.classList.add("fa-ellipsis-v");
+	vEllipsis.setAttribute("aria-hidden","true");
+	dropdown.appendChild(vEllipsis);
+
+	//add content menu
+	dropdownContent = document.createElement("div");
+	dropdownContent.classList.add("dropdown-content");
+	dropdown.appendChild(dropdownContent);
+
+	//add links 
+	aLinkReload = document.createElement("a");
+	aLinkReload.classList.add("reload");
+	aLinkReload.textContent = "Restart";
+	dropdownContent.appendChild(aLinkReload);
+
+
+	aLinkRemove = document.createElement("a");
+	aLinkRemove.classList.add("remove");
+	aLinkRemove.textContent = "Remove";
+	dropdownContent.appendChild(aLinkRemove);
+
+	aLinkReload.addEventListener("click",restartThing);
+	aLinkRemove.addEventListener("click", removeThing);
+}
+
+//Adds newly created pros to the list
 function buildProList() {
 	
 	console.log("fn buildProList");
@@ -64,6 +236,7 @@ function buildProList() {
 	proList.appendChild(li);
 }
 
+//Adds newly created cons to the list
 function buildConList(){
 	console.log("fn buildConList");
 	//create li 
@@ -71,6 +244,124 @@ function buildConList(){
 	li.textContent = conInput.value;
 
 	conList.appendChild(li);
+}
+
+//Load glory items stored in glory array 
+
+function sendToGlory(){
+	console.log("fn send to glory");  
+	thingToLoad.friend_status = true;
+	gloryPedestal.push(thingToLoad);
+
+	localStorage.setItem('gloryPedestal', JSON.stringify(gloryPedestal));
+	buildGloryPedestal();
+	loadThing();
+}
+
+function sendToHatePit() {
+	console.log("fn send to hate pit");
+	thingToLoad.foe_status = true;
+	hatePit.push(thingToLoad);
+
+	localStorage.setItem("hatePit", JSON.stringify(hatePit));
+	buildHatePit();
+	loadThing();
+}
+
+//build glory list 
+function buildGloryPedestal() {
+	console.log("fn buildGloryPedestal");
+
+	//create li
+	li = document.createElement("li");
+	li.textContent = thingToLoad.name;
+	pedestalList.appendChild(li);
+
+	//add dropdown
+	dropdown = document.createElement("div");
+	dropdown.classList.add("dropdown");
+	li.appendChild(dropdown);
+
+
+	//add vertical ellipsis 
+	vEllipsis = document.createElement("i"); 
+	vEllipsis.classList.add("fa");
+	vEllipsis.classList.add("fa-ellipsis-v");
+	vEllipsis.setAttribute("aria-hidden","true");
+	dropdown.appendChild(vEllipsis);
+
+	//add content menu
+	dropdownContent = document.createElement("div");
+	dropdownContent.classList.add("dropdown-content");
+	dropdown.appendChild(dropdownContent);
+
+	//add links 
+	aLinkReload = document.createElement("a");
+	aLinkReload.classList.add("reload");
+	aLinkReload.textContent = "Restart";
+	dropdownContent.appendChild(aLinkReload);
+
+
+	aLinkRemove = document.createElement("a");
+	aLinkRemove.classList.add("remove");
+	aLinkRemove.textContent = "Remove";
+	dropdownContent.appendChild(aLinkRemove);
+
+	aLinkReload.addEventListener("click",restartThing);
+	aLinkRemove.addEventListener("click", removeThing);
+
+	console.log(thingToLoad.friend_status);
+
+}
+
+function buildHatePit(){
+	//create li
+	li = document.createElement("li");
+	li.textContent = thingToLoad.name;
+	hatePitList.appendChild(li);
+
+	//add dropdown
+	dropdown = document.createElement("div");
+	dropdown.classList.add("dropdown");
+	li.appendChild(dropdown);
+
+	//add vertical ellipsis 
+	vEllipsis = document.createElement("i"); 
+	vEllipsis.classList.add("fa");
+	vEllipsis.classList.add("fa-ellipsis-v");
+	vEllipsis.setAttribute("aria-hidden","true");
+	dropdown.appendChild(vEllipsis);
+
+	//add content menu
+	dropdownContent = document.createElement("div");
+	dropdownContent.classList.add("dropdown-content");
+	dropdown.appendChild(dropdownContent);
+
+	//add links 
+	aLinkReload = document.createElement("a");
+	aLinkReload.classList.add("reload");
+	aLinkReload.textContent = "Restart";
+	dropdownContent.appendChild(aLinkReload);
+
+
+	aLinkRemove = document.createElement("a");
+	aLinkRemove.classList.add("remove");
+	aLinkRemove.textContent = "Remove";
+	dropdownContent.appendChild(aLinkRemove);
+
+	aLinkReload.addEventListener("click",restartThing);
+	aLinkRemove.addEventListener("click", removeThing);
+   
+	console.log(thingToLoad.foe_status);
+}
+
+
+function restartThing(e){
+	console.log("fn restartThing", e.target);
+}
+
+function removeThing(){
+	console.log("fn removeThing");
 }
 
 
